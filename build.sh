dc34defb400e90416dac2b4ea5dadd4c9b428e0c
Mustafa Ozan Alpay <frozsgy@gmail.com>
2023-12-17 20:01:50 +0300

    add heroku procfile configuration

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ mkdir gezenti
 cp ./data/game.py ./gezenti/game.py
 cp ./data/permutations_shuffled.json ./gezenti/permutations_shuffled.json
 cp ./data/cities.json ./gezenti/cities.json
-cp ./data/backend.py ./gezenti/run.py
+cp ./data/backend.py ./gezenti/app.py
 cp ./data/requirements.txt ./gezenti/requirements.txt
 
 # copy frontend
 cp -r ./frontend/ ./gezenti/static/
 rm -f ./gezenti/README.md
 
-
+# copy heroku
+cp ./Procfile ./gezenti/Procfile
