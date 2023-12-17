cb8859559a364f7a67574f606398ef70b9015fe2
Mustafa Ozan Alpay <frozsgy@gmail.com>
2023-12-17 19:33:25 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir gezenti
+
+# copy backend
+cp ./data/game.py ./gezenti/game.py
+cp ./data/permutations_shuffled.json ./gezenti/permutations_shuffled.json
+cp ./data/cities.json ./gezenti/cities.json
+cp ./data/backend.py ./gezenti/run.py
+cp ./data/requirements.txt ./gezenti/requirements.txt
+
+# copy frontend
+cp -r ./frontend/ ./gezenti/static/
+rm -f ./gezenti/README.md
+
+
