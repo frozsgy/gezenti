af762894efd9c38cbd801427456046366f95ce97
Mustafa Ozan Alpay <frozsgy@gmail.com>
2024-06-20 21:42:22 +0300

    fix faulty neighbour data for zonguldak & çankırı

--- data/shuffle_permutations.py
+++ data/shuffle_permutations.py
@@ -9,5 +9,21 @@ def read_from_json():
         return permutations
 
 
+def read_from_json_while_preserving_original_order():
+    response = []
+    with open('permutations.json', 'r') as f:
+        permutations = json.load(f)
+        with open('permutations_shuffled.json.old', 'r') as fs:
+            shuffled_permutations = json.load(fs)
+
+            for entry in shuffled_permutations:
+                origin = entry['origin']
+                destination = entry['destination']
+
+                q = next(x for x in permutations if x['origin'] == origin and x['destination'] == destination)
+                response.append(q)
+
+        return response
+
 if __name__ == '__main__':
-    print(str(json.dumps(read_from_json(), default=vars, ensure_ascii=False)))
+    print(str(json.dumps(read_from_json_while_preserving_original_order(), default=vars, ensure_ascii=False)))
